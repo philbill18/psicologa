da3af22d7b9e6988576fb850b181c39f757188c3
Philipp Noller <philipp.noller@outlook.de>
2022-01-27 19:20:25 +0100

    new bg and style in red

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// window.addEventListener("load", function(){
+//     var header = document.getElementsByTagName("header")[0].clientHeight;
+//     var full = document.body.clientHeight;
+    
+//     var sollHeight = full - header;
+//     document.getElementsByTagName("main")[0].style.height = sollHeight+"px";
+// })
