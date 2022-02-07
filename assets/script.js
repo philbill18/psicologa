31a844d6aad31f169e45861a5d6d5e49e28a0452
Philipp Noller <philipp.noller@outlook.de>
2022-02-07 22:39:57 +0100

    added version 2

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,76 @@
-// window.addEventListener("load", function(){
-//     var header = document.getElementsByTagName("header")[0].clientHeight;
-//     var full = document.body.clientHeight;
+// window.addEventListener('DOMContentLoaded', switchLanguage);
+
+// function switchLanguage(){
+//   if (navigator.language){
+//     var lang = navigator.language
+//     if(lang.startsWith("es")){
+//       console.log("es");
     
-//     var sollHeight = full - header;
-//     document.getElementsByTagName("main")[0].style.height = sollHeight+"px";
-// })
+//     } else {
+//       console.log("en");
+//       if(!document.referrer.includes("psicologa")){
+//         location.href = "en.html" ;
+//       }
+//       //location.href = "en.html" ;
+     
+//     }
+//   }
+
+
+// }
+
+function switchToEnglish(){
+
+  location.href = "en.html"
+}
+
+function switchToSpanish(){
+
+  location.href = "es.html"
+}
+
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+function scrollToTargetAdjusted(elem) {
+  
+  console.log(elem);
+  var ver2 = elem.id.split("To").pop();
+  //var id = elem.href.split("#").pop();
+  var element = document.getElementById(ver2);
+  var headerOffset = 45+100;
+  var elementPosition = element.getBoundingClientRect().top;
+  var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+  window.scrollTo({
+    top: offsetPosition,
+    behavior: "smooth"
+  });
+}
+
+function scrollToTargetAdjustedMobile(elem) {
+
+  closeNav();
+  
+  console.log(elem);
+  var ver2 = elem.id.split("Mobile").pop();
+  if(ver2 == ""){
+    ver2 = "wave"
+  }
+  //var id = elem.href.split("#").pop();
+  var element = document.getElementById(ver2);
+  var headerOffset = 45+100;
+  var elementPosition = element.getBoundingClientRect().top;
+  var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+  window.scrollTo({
+    top: offsetPosition,
+    behavior: "smooth"
+  });
+}
